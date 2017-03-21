714765fd9338d655a2fe030679c35d124d94c932
Hongwei Peng <pengisgood@gmail.com>
2017-03-21 17:52:13 +0800

    OLMIS-2148 bring user back

--- src/physical-inventory/physical-inventory.routes.js
+++ src/physical-inventory/physical-inventory.routes.js
@@ -44,6 +44,9 @@
         facility: function (facilityFactory) {
           return facilityFactory.getUserHomeFacility();
         },
+        user: function (authorizationService) {
+          return authorizationService.getUser();
+        },
         programs: function (programService, user) {
           return programService.getUserPrograms(user.user_id, true);
         }
