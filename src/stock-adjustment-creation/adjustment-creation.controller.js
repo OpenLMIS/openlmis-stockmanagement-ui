f0dda58f5b2f380393a79525c57b3bb96d440f89
Yi Deng <dydyistc@gmail.com>
2017-05-02 14:07:12 +0800

    OLMIS-2361 reorder items by lot ID when support lot

--- src/stock-adjustment-creation/adjustment-creation.controller.js
+++ src/stock-adjustment-creation/adjustment-creation.controller.js
@@ -183,6 +183,9 @@
       var sorted = $filter('orderBy')(vm.addedLineItems, ['orderable.productCode', '-occurredDate']);
 
       vm.displayItems = _.chain(sorted).groupBy(function (item) {
+        if (vm.hasLot) {
+          return item.lot ? item.lot.id : item.orderable.id;
+        }
         return item.orderable.id
       }).sortBy(function (group) {
         return _.every(group, function (item) {
