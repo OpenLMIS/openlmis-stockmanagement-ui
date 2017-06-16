8c2ed9802335cd3ba143b789e96b98a71a0b4842
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2017-06-16 13:06:10 +0200

    OLMIS-2654: added tests for vvm status constant

---
+++ src/stock-physical-inventory-draft/vvm-statuses.constant.spec.js
@@ -0,0 +1,37 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+describe("VVM_STATUS", function() {
+
+    var VVM_STATUS;
+
+    beforeEach(function() {
+        module('stock-physical-inventory-draft');
+
+        inject(function($injector) {
+            VVM_STATUS = $injector.get('VVM_STATUS');
+        });
+    });
+
+    describe('getDisplayName', function() {
+        it('should get display name for STAGE_1 status', function() {
+            expect(VVM_STATUS.$getDisplayName(VVM_STATUS.STAGE_1)).toBe('stockPhysicalInventoryDraft.stage1');
+        });
+
+        it('should get display name for STAGE_1 status', function() {
+            expect(VVM_STATUS.$getDisplayName(VVM_STATUS.STAGE_2)).toBe('stockPhysicalInventoryDraft.stage2');
+        });
+    });
+});
