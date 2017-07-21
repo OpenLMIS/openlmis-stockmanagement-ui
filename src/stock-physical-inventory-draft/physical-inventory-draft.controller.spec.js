feb519f78a943c34ef4b6a548d8b0a06cd418b2a
Nikodem Graczewski <ngraczewski@soldevelo.com>
2017-07-21 14:59:44 +0200

    OLMIS-2711: Fixed indentation in the file

--- src/stock-physical-inventory-draft/physical-inventory-draft.controller.spec.js
+++ src/stock-physical-inventory-draft/physical-inventory-draft.controller.spec.js
@@ -13,189 +13,207 @@
  * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
  */
 
-describe("PhysicalInventoryDraftController", function () {
+describe("PhysicalInventoryDraftController", function() {
 
-  var vm, q, $rootScope, scope, state, stateParams,
-    addProductsModalService, draftFactory, chooseDateModalService,
-    facility, program, draft, lineItem1, lineItem2, lineItem3, lineItem4, reasons;
+    var vm, q, $rootScope, scope, state, stateParams,
+        addProductsModalService, draftFactory, chooseDateModalService,
+        facility, program, draft, lineItem1, lineItem2, lineItem3, lineItem4, reasons;
 
-  beforeEach(function () {
+    beforeEach(function() {
 
-    module('stock-physical-inventory-draft');
+        module('stock-physical-inventory-draft');
 
-    inject(function($injector) {
-      $q = $q;
-      $rootScope = $injector.get('$rootScope');
-      scope = $rootScope.$new();
-      state = jasmine.createSpyObj('$state', ['go']);
-      chooseDateModalService = jasmine.createSpyObj('chooseDateModalService', ['show']);
-      state.current = {name: '/a/b'};
+        inject(function($injector) {
+            $q = $q;
+            $rootScope = $injector.get('$rootScope');
+            scope = $rootScope.$new();
+            state = jasmine.createSpyObj('$state', ['go']);
+            chooseDateModalService = jasmine.createSpyObj('chooseDateModalService', ['show']);
+            state.current = {
+                name: '/a/b'
+            };
 
-      addProductsModalService = $injector.get('addProductsModalService');
-      spyOn(addProductsModalService, 'show');
+            addProductsModalService = $injector.get('addProductsModalService');
+            spyOn(addProductsModalService, 'show');
 
-      draftFactory = $injector.get('physicalInventoryDraftFactory');
+            draftFactory = $injector.get('physicalInventoryDraftFactory');
 
-      program = {
-        name: 'HIV',
-        id: '1'
-      };
+            program = {
+                name: 'HIV',
+                id: '1'
+            };
 
-      facility = {
-        id: "10134",
-        name: "National Warehouse",
-      };
+            facility = {
+                id: "10134",
+                name: "National Warehouse",
+            };
 
-      stateParams = {};
+            stateParams = {};
 
-      lineItem1 = {
-        quantity: 1,
-        orderable: {
-          productCode: 'C100',
-          fullProductName: 'a'
-        },
-        stockAdjustments: [{
-          quantity: 1,
-          reason: {
-            reasonType: 'CREDIT'
-          }
-        }]
-      };
+            lineItem1 = {
+                quantity: 1,
+                orderable: {
+                    productCode: 'C100',
+                    fullProductName: 'a'
+                },
+                stockAdjustments: [{
+                    quantity: 1,
+                    reason: {
+                        reasonType: 'CREDIT'
+                    }
+                }]
+            };
 
-      lineItem2 = {
-        quantity: null,
-        orderable: {
-          productCode: 'C300',
-          fullProductName: 'b'
-        },
-        stockAdjustments: []
-      };
+            lineItem2 = {
+                quantity: null,
+                orderable: {
+                    productCode: 'C300',
+                    fullProductName: 'b'
+                },
+                stockAdjustments: []
+            };
 
-      lineItem3 = {
-        quantity: null,
-        isAdded: true,
-        orderable: {
-          productCode: 'C200',
-          fullProductName: 'c'
-        },
-        lot: {
-          lotCode: 'LC0001',
-          expirationDate: ''
-        },
-        stockAdjustments: []
-      };
+            lineItem3 = {
+                quantity: null,
+                isAdded: true,
+                orderable: {
+                    productCode: 'C200',
+                    fullProductName: 'c'
+                },
+                lot: {
+                    lotCode: 'LC0001',
+                    expirationDate: ''
+                },
+                stockAdjustments: []
+            };
 
-      lineItem4 = {
-        quantity: null,
-        orderable: {
-          productCode: 'C300',
-          fullProductName: 'b'
-        },
-        lot: {
-          lotCode: 'L1'
-        },
-        stockAdjustments: []
-      };
+            lineItem4 = {
+                quantity: null,
+                orderable: {
+                    productCode: 'C300',
+                    fullProductName: 'b'
+                },
+                lot: {
+                    lotCode: 'L1'
+                },
+                stockAdjustments: []
+            };
 
-      draft = {
-          lineItems: [
-            lineItem1, lineItem2, lineItem3, lineItem4
-          ]
-      };
+            draft = {
+                lineItems: [
+                    lineItem1, lineItem2, lineItem3, lineItem4
+                ]
+            };
 
-      reasons = [{
-          name: 'Reason one'
-      }, {
-          name: 'Reason two'
-      }];
+            reasons = [{
+                name: 'Reason one'
+            }, {
+                name: 'Reason two'
+            }];
 
-      vm = $injector.get('$controller')('PhysicalInventoryDraftController', {
-        facility: facility,
-        program: program,
-        $state: state,
-        $scope: scope,
-        $stateParams: stateParams,
-        displayLineItemsGroup: [[lineItem1], [lineItem3]],
-        draft: draft,
-        addProductsModalService: addProductsModalService,
-        chooseDateModalService: chooseDateModalService,
-        reasons: reasons
-      });
+            vm = $injector.get('$controller')('PhysicalInventoryDraftController', {
+                facility: facility,
+                program: program,
+                $state: state,
+                $scope: scope,
+                $stateParams: stateParams,
+                displayLineItemsGroup: [
+                    [lineItem1],
+                    [lineItem3]
+                ],
+                draft: draft,
+                addProductsModalService: addProductsModalService,
+                chooseDateModalService: chooseDateModalService,
+                reasons: reasons
+            });
+        });
     });
-  });
 
-  it("should init displayLineItemsGroup and sort by product code properly", function () {
-    expect(vm.displayLineItemsGroup).toEqual([[lineItem1], [lineItem3]]);
-  });
+    it("should init displayLineItemsGroup and sort by product code properly", function() {
+        expect(vm.displayLineItemsGroup).toEqual([
+            [lineItem1],
+            [lineItem3]
+        ]);
+    });
 
-  it("should reload with page and keyword when search", function () {
-    vm.keyword = '200';
-    vm.search();
+    it("should reload with page and keyword when search", function() {
+        vm.keyword = '200';
+        vm.search();
 
-    var params = {
-      page: 0,
-      keyword: '200',
-      program: program,
-      programId: '1',
-      facility: facility,
-      draft: draft
-    };
+        var params = {
+            page: 0,
+            keyword: '200',
+            program: program,
+            programId: '1',
+            facility: facility,
+            draft: draft
+        };
 
-    expect(state.go).toHaveBeenCalledWith('/a/b', params, {reload: '/a/b'})
-  });
+        expect(state.go).toHaveBeenCalledWith('/a/b', params, {
+            reload: '/a/b'
+        })
+    });
 
-  it("should only pass items not added yet to add products modal", function () {
-    var deferred = $q.defer();
-    deferred.resolve();
-    addProductsModalService.show.andReturn(deferred.promise);
+    it("should only pass items not added yet to add products modal", function() {
+        var deferred = $q.defer();
+        deferred.resolve();
+        addProductsModalService.show.andReturn(deferred.promise);
 
-    vm.addProducts();
-    expect(addProductsModalService.show).toHaveBeenCalledWith([lineItem2, lineItem4], true);
-  });
+        vm.addProducts();
+        expect(addProductsModalService.show).toHaveBeenCalledWith([lineItem2, lineItem4], true);
+    });
 
-  it('should save draft', function () {
-    spyOn(draftFactory, 'saveDraft');
-    draftFactory.saveDraft.andReturn($q.defer().promise);
-    $rootScope.$apply();
+    it('should save draft', function() {
+        spyOn(draftFactory, 'saveDraft');
+        draftFactory.saveDraft.andReturn($q.defer().promise);
+        $rootScope.$apply();
 
-    vm.saveDraft();
-    expect(draftFactory.saveDraft).toHaveBeenCalledWith(draft);
-  });
+        vm.saveDraft();
+        expect(draftFactory.saveDraft).toHaveBeenCalledWith(draft);
+    });
 
-  it('should highlight empty quantities before submit', function () {
-    vm.submit();
-    expect(lineItem1.quantityInvalid).toBeFalsy();
-    expect(lineItem3.quantityInvalid).toBeTruthy();
-  });
+    it('should highlight empty quantities before submit', function() {
+        vm.submit();
+        expect(lineItem1.quantityInvalid).toBeFalsy();
+        expect(lineItem3.quantityInvalid).toBeTruthy();
+    });
 
-  it('should not show modal for occurred date if any quantity missing', function () {
-    vm.submit();
-    expect(chooseDateModalService.show).not.toHaveBeenCalled();
-  });
+    it('should not show modal for occurred date if any quantity missing', function() {
+        vm.submit();
+        expect(chooseDateModalService.show).not.toHaveBeenCalled();
+    });
 
-  it('should show modal for occurred date if no quantity missing', function () {
-    lineItem3.quantity = 123;
-    lineItem3.stockAdjustments = [{
-      quantity: 123,
-      reason: {
-        reasonType: 'CREDIT'
-      }
-    }]
-    var deferred = $q.defer();
-    deferred.resolve();
-    chooseDateModalService.show.andReturn(deferred.promise);
+    it('should show modal for occurred date if no quantity missing', function() {
+        lineItem3.quantity = 123;
+        lineItem3.stockAdjustments = [{
+            quantity: 123,
+            reason: {
+                reasonType: 'CREDIT'
+            }
+        }]
+        var deferred = $q.defer();
+        deferred.resolve();
+        chooseDateModalService.show.andReturn(deferred.promise);
 
-    vm.submit();
+        vm.submit();
 
-    expect(chooseDateModalService.show).toHaveBeenCalled();
-  });
+        expect(chooseDateModalService.show).toHaveBeenCalled();
+    });
 
-  it('should aggregate given field values', function () {
-    var lineItem1 = {id: "1", quantity: 2, stockOnHand: 233};
-    var lineItem2 = {id: "2", quantity: 1, stockOnHand: null};
-    var lineItems = [lineItem1, lineItem2];
+    it('should aggregate given field values', function() {
+        var lineItem1 = {
+            id: "1",
+            quantity: 2,
+            stockOnHand: 233
+        };
+        var lineItem2 = {
+            id: "2",
+            quantity: 1,
+            stockOnHand: null
+        };
+        var lineItems = [lineItem1, lineItem2];
 
-    expect(vm.calculate(lineItems, 'quantity')).toEqual(3);
-    expect(vm.calculate(lineItems, 'stockOnHand')).toEqual(233);
-  });
+        expect(vm.calculate(lineItems, 'quantity')).toEqual(3);
+        expect(vm.calculate(lineItems, 'stockOnHand')).toEqual(233);
+    });
 });
