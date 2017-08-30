cb1813ad4a9d0f1c8b73c6aeeb89682ed22f670a
Nick Reid <nickreid@nickreid.com>
2017-08-29 20:13:51 -0700

    OLMIS-3042: Test to make stock-reasons an input-control

---
+++ src/stock-reasons/stock-reasons-popover-compile.directive.spec.js
@@ -0,0 +1,36 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+describe('stockReasons popover compile', function() {
+
+    beforeEach(module('stock-reasons'));
+
+    it('adds popover and input-control controllers to element', inject(function($rootScope, $compile){
+        var element,
+            scope = $rootScope.$new(),
+            html = '<stock-reasons ng-model="adjustments" line-item="lineItem" reasons="reasons" is-disabled="isDisabled" />';
+
+        scope.adjustments = [];
+        scope.lineItem = {};
+        scope.reasons = [];
+        scope.isDisabled = false;
+
+        element = $compile(html)(scope);
+
+        expect(element.attr('input-control')).not.toBeUndefined();
+        expect(element.attr('popover')).not.toBeUndefined();
+    }));
+
+});
