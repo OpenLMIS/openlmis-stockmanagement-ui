0003bb7a6b53a76a4d0ad25e016efb478627d4e8
Klaudia Pałkowska <kpalkowska@soldevelo.com>
2018-05-08 10:37:23 +0200

    OLMIS-4623: Added tests for constants

---
+++ src/stock-constants/reason-categories.constant.spec.js
@@ -0,0 +1,53 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+describe('REASON_CATEGORIES', function() {
+
+    var REASON_CATEGORIES;
+
+    beforeEach(function() {
+        module('stock-constants');
+
+        inject(function($injector) {
+            REASON_CATEGORIES = $injector.get('REASON_CATEGORIES');
+        });
+    });
+
+    describe('getLabel', function() {
+
+        it('should return label for TRANSFER status', function() {
+            expect(REASON_CATEGORIES.getLabel(REASON_CATEGORIES.TRANSFER)).toBe('stockConstants.transfer');
+        });
+
+        it('should return label for ADJUSTMENT status', function() {
+            expect(REASON_CATEGORIES.getLabel(REASON_CATEGORIES.ADJUSTMENT)).toBe('stockConstants.adjustment');
+        });
+
+        it('should return label for PHYSICAL_INVENTORY status', function() {
+            expect(REASON_CATEGORIES.getLabel(REASON_CATEGORIES.PHYSICAL_INVENTORY)).toBe('stockConstants.physicalInventory');
+        });
+    });
+
+    describe('getCategories', function() {
+
+        it('should return all reason categories', function() {
+            expect(REASON_CATEGORIES.getCategories()).toEqual([
+                REASON_CATEGORIES.TRANSFER,
+                REASON_CATEGORIES.ADJUSTMENT,
+                REASON_CATEGORIES.PHYSICAL_INVENTORY
+            ]);
+        });
+    });
+});
---
+++ src/stock-constants/reason-types.constant.spec.js
@@ -0,0 +1,53 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+describe('REASON_TYPES', function() {
+
+    var REASON_TYPES;
+
+    beforeEach(function() {
+        module('stock-constants');
+
+        inject(function($injector) {
+            REASON_TYPES = $injector.get('REASON_TYPES');
+        });
+    });
+
+    describe('getLabel', function() {
+
+        it('should return label for CREDIT status', function() {
+            expect(REASON_TYPES.getLabel(REASON_TYPES.CREDIT)).toBe('stockConstants.credit');
+        });
+
+        it('should return label for DEBIT status', function() {
+            expect(REASON_TYPES.getLabel(REASON_TYPES.DEBIT)).toBe('stockConstants.debit');
+        });
+
+        it('should return label for BALANCE_ADJUSTMENT status', function() {
+            expect(REASON_TYPES.getLabel(REASON_TYPES.BALANCE_ADJUSTMENT)).toBe('stockConstants.balanceAdjustment');
+        });
+    });
+
+    describe('getTypes', function() {
+
+        it('should return all reason types', function() {
+            expect(REASON_TYPES.getTypes()).toEqual([
+                REASON_TYPES.CREDIT,
+                REASON_TYPES.DEBIT,
+                REASON_TYPES.BALANCE_ADJUSTMENT
+            ]);
+        });
+    });
+});
