8264ab0a4c3648150362533284b223684f2535a1
sniedzielski <sniedzielski@soldevelo.com>
2022-07-04 11:15:51 +0200

    MW-1147: added missing license information

--- src/stock-adjustment-mobile/components/toast.component.jsx
+++ src/stock-adjustment-mobile/components/toast.component.jsx
@@ -1,3 +1,19 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+
 import React, { useEffect } from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import { setToastList } from '../reducers/toasts';
