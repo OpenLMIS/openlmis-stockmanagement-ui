83514d2250db3ebb3b426efa8740ad119f3d9383
kszymankiewiczsd <kszymankiewicz@soldevelo.com>
2025-10-30 13:51:22 +0100

    Revert lowering quantity max input

--- src/stock-adjustment-creation/adjustment-creation.controller.js
+++ src/stock-adjustment-creation/adjustment-creation.controller.js
@@ -280,7 +280,7 @@
                     && lineItem.reason.reasonType === REASON_TYPES.DEBIT) {
                 lineItem.$errors.quantityInvalid = messageService
                     .get('stockAdjustmentCreation.quantityGreaterThanStockOnHand');
-            } else if (lineItem.quantity > 1000000) {
+            } else if (lineItem.quantity > MAX_INTEGER_VALUE) {
                 lineItem.$errors.quantityInvalid = messageService.get('stockmanagement.numberTooLarge');
             } else if (lineItem.quantity >= 1) {
                 lineItem.$errors.quantityInvalid = false;
